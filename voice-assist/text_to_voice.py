17482adce404bf83efcfe7f3c238f821dd19ca4b
Nick <91033069+nicholas-buscaglia@users.noreply.github.com>
2023-04-04 20:04:09 -0400

    Create text_to_voice.py

---
+++ voice-assist/text_to_voice.py
@@ -0,0 +1,38 @@
+import pyttsx3
+
+# 11 (hawking)
+# 17 (australian female) winner
+# 26 (femaale)
+# 32 (male)
+# 33 (femaale)
+
+
+def speak(something):
+
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')  # Get list of available voices
+    engine.setProperty('voice', voices[17].id)  # Set voice to Karen's voice
+
+    # Set a slower speaking rate
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-25)
+
+    engine.say(something)
+
+    engine.runAndWait()
+
+
+# import pyttsx3
+# engine = pyttsx3.init()
+# voices = engine.getProperty('voices')
+# for voice in voices:
+#     print(voice, voice.id)
+#     engine.setProperty('voice', voice.id)
+#     engine.say("Hello World!")
+#     engine.runAndWait()
+#     engine.stop()
+
+    # Daniel
+    # Fred (hawking)
+    # Karen
+    # Alex
