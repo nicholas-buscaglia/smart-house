728d7f1f24da240e9f005f865577b6bc8c0e36a0
Nick Buscaglia <buscaglia.nick@gmail.com>
2023-04-05 14:10:01 -0400

    Added new files/changes to smart-house project

--- voice-assist/main.py
+++ voice-assist/main.py
@@ -5,25 +5,19 @@ import text_to_voice
 
 
 def voice_assist():
-
     while True:
-
         wakeword.poppy()
         convo = 'new'
         text_to_voice.speak("Greetings human...how may I assist you?")
 
-        prompt = voice_to_text.get_prompt_string()
-        if prompt != '':
-            response = whisper.chat(prompt, convo)
-            text_to_voice.speak(response)
-
-        # Continue conversation
-        while prompt != '':
+        while True:
             prompt = voice_to_text.get_prompt_string()
             if prompt != '':
-                convo = 'continue'
                 response = whisper.chat(prompt, convo)
                 text_to_voice.speak(response)
+                convo = 'continue'
+            else:
+                break
 
 
 if __name__ == '__main__':
