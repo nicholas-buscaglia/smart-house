ba2afc5cacdf189907387e9336f55bbfec55cc09
Nick <91033069+nicholas-buscaglia@users.noreply.github.com>
2023-04-04 20:02:22 -0400

    Create main.py

---
+++ voice-assist/main.py
@@ -0,0 +1,22 @@
+import wakeword
+import whisper
+import voice_to_text
+import text_to_voice
+
+
+def voice_assist():
+
+    while True:
+
+        wakeword.jarvis()
+        text_to_voice.speak("Greetings human...how may I assist you?")
+
+        prompt = voice_to_text.get_prompt_string()
+        if prompt != '':
+            print(f'You:\n{prompt}')
+            response = whisper.chat(prompt)
+            text_to_voice.speak(response)
+
+
+if __name__ == '__main__':
+    voice_assist()
